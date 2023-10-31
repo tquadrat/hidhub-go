d19f749170a5b4db010166417f5de3b3092d440b
Thomas Thrien <thomas.thrien@tquadrat.org>
2023-10-31 16:22:50 +0100

    Added some basic USB functionality to project

--- hidhub.go
+++ hidhub.go
@@ -2,8 +2,27 @@ package main
 
 import "fmt"
 import "rsc.io/quote"
+import "github.com/sstallion/go-hid" // package hid
 
 func main() {
     fmt.Println( "Hello, World!" )
     fmt.Println( quote.Go() )
-}
+    
+    //---* Initialise HIDAPI *-------------------------------------------------
+    fmt.Println( "HIDAPI Version: ", hid.GetVersionStr() )
+    hid.Init()
+    
+    //---* Dump the connected HID deviced *------------------------------------
+    hid.Enumerate( hid.VendorIDAny, hid.ProductIDAny, func( info *hid.DeviceInfo ) error {
+	fmt.Printf( "%s: ID %04x:%04x '%s' '%s'\n",
+		info.Path,
+		info.VendorID,
+		info.ProductID,
+		info.MfrStr,
+		info.ProductStr )
+	return nil
+    })
+    
+    //---* Shutdown the HIDAPI *-----------------------------------------------
+    hid.Exit()
+}   //  main()
